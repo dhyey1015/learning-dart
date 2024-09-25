4e08efed1df1b8c2b5996b183ee977acc8d1a344
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-09-26 03:56:49 +0530

    learned about something called records which helps to return multiple returns

---
+++ return-multiple-datatypes-functions.dart
@@ -0,0 +1,8 @@
+void main() {
+  print(multipleReturnFunction()); //to print all the return values
+  print(multipleReturnFunction().$2); //to print return individual return
+}
+
+(int, String, double) multipleReturnFunction() {
+  return (2, "Hello world!!", 2.50);
+}
