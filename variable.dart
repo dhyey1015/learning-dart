597f2e32f8467f3ed50fc98a336c1dcf766eb814
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-09-25 22:07:57 +0530

    learned variables

---
+++ variable.dart
@@ -0,0 +1,58 @@
+void main() {
+  //int datatype
+  print("int datatype:");
+  int first = 4;
+  int second = 2;
+  print(first);
+  print(second);
+  print(first + second);
+  print(first * second);
+  print(first - second);
+  //end int datatype
+
+  print("");
+  //double datatype ----> similar to float datatype
+  print("double datatype:");
+  double third = 4.40;
+  double fourth = 2.20;
+  print(third);
+  print(fourth);
+  print(third + fourth);
+  print(third / fourth);
+  print(third - fourth);
+  // end double datatype
+  print("");
+
+  //string datatype
+  print("string datatype:");
+  String strfirst = "Hello, ";
+  String strsecond = "world!!";
+
+  print(strfirst);
+  print(strsecond);
+  print(strfirst + strsecond);
+  //end string datatype
+
+  print("");
+
+  //boolean datatype bool
+  print("boolean datatype:");
+  bool a = false;
+  bool b = true;
+  print(a);
+  print(b);
+  //end boolean datatype
+
+  //dynamic datatype
+  print("dynamic datatype");
+  dynamic integer = 2;
+  dynamic decimal = 2.2;
+  dynamic string = "Hello";
+  dynamic boolean = true; //or false
+
+  print(integer);
+  print(decimal);
+  print(string);
+  print(boolean);
+  //end dynamic datatye
+}
