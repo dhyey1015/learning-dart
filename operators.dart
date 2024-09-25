b8aeb69cc7826d4d0d7026a42d3e15bce7d2e9e9
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-09-25 21:29:19 +0530

    learned operators

---
+++ operators.dart
@@ -0,0 +1,12 @@
+void main() {
+  print(3 + 1);
+  // can't do print('3' + 1);
+  print('3' + 'a');
+  print('3' + '1');
+  print('3' * 2);
+  /* 
+  notes
+  we cannot use other operators here instead of '+' for 2 strings
+  can't do print('3' / '1');
+  */
+}
--- print.dart
+++ print.dart
@@ -1,3 +1,7 @@
-void main(){
-  print("Hello, World!");
+void main() {
+  print("Hello, World!"); //for strings
+  print(9); //for int
 }
+//printing num and string are different things
+//postional args not defined
+//using not defined variable name not defined
