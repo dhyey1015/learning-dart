a9a5d3da84d2efb076de40f580512ba01b2238d1
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-09-26 03:39:03 +0530

    learned function

---
+++ function.dart
@@ -0,0 +1,28 @@
+void main() {
+  print("------------------------");
+
+  print("print hello world function");
+  print(printHelloWorld);
+  printHelloWorld();
+  print(printHelloWorld());
+
+  print("------------------------");
+
+  print("arithmetic function");
+  arithmeticOps(2, 4);
+  print(arithmeticOps(2, 4));
+
+  print("------------------------");
+}
+
+String printHelloWorld() {
+  print("Hello, World!");
+  return "Hello!!";
+}
+
+double arithmeticOps(int a, int b) {
+  print(a + b);
+  print(a - b);
+  double c = a / b;
+  return c;
+}
