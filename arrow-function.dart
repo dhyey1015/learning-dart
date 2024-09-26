230956a5b391f227148e9a3276623f977cde0cfb
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-09-26 12:41:55 +0530

    added

--- arrow-function.dart
+++ arrow-function.dart
@@ -1,16 +1,19 @@
 void main() {
   arrowFn();
+
   // var fn = arrowFn1();
-  // print(fn);
+  print(arrowFn1());
+
+  //diff thing not much related to arrow function example for just we can using it in main function too
+  var cal = (int num) => num * num;
+  print(cal(3));
 }
 
 //can do this only
-void arrowFn() => print("hello"); 
+void arrowFn() => print("hello");
 
+String arrowFn1() => "hello";
 
-// void arrowFn() => 
+// void arrowFn() =>
 // print("hello");  //----------> can't do this too
 // print("heelo");
-  
-
-// String arrowFn1() => return "hello"; // can't do this
---
+++ no-name-function.dart
@@ -0,0 +1,9 @@
+void main() {
+  var noNameFn = () => "hello";
+  print(noNameFn());
+}
+
+// can't define anonymus function out of the main
+// void (){
+//   print("something");
+// } 
