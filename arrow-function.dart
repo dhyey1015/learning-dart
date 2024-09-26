356291488b6cf08984236e587e05a970ae22c150
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-09-26 12:25:02 +0530

    learned arrow functions

---
+++ arrow-function.dart
@@ -0,0 +1,16 @@
+void main() {
+  arrowFn();
+  // var fn = arrowFn1();
+  // print(fn);
+}
+
+//can do this only
+void arrowFn() => print("hello"); 
+
+
+// void arrowFn() => 
+// print("hello");  //----------> can't do this too
+// print("heelo");
+  
+
+// String arrowFn1() => return "hello"; // can't do this
